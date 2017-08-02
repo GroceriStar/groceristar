b14609894725d7787cd0974d2edc3d09e6615e11
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-08-02 22:31:40 +0300

    +

--- bin/import/departments.js
+++ bin/import/departments.js
@@ -8,16 +8,133 @@ var relation2    = 'departmentIds';
 
 function getDepartments(){
 
+[
+	'',
+	'',
+	'',
+	'',
+	'',
+	'',
+	'',
+	'',
+	''
+];
+
 	var departments = [
 		{
 	
-		 name: "BAKERY/BREAD",
-		 
+		 name: "Fresh vegetables",
+		 type: 'food'
 		},
 		{
-		
-			name: "BAKING",
-			
+	
+		 name: "Condiments / Sauces",
+		 type: 'food'
+		},
+		{
+	
+		 name: "Dairy",
+		 type: 'food'
+		},{
+	
+		 name: "Cheese",
+		 type: 'food'
+		},{
+	
+		 name: "Meat",
+		 type: 'food'
+		},{
+	
+		 name: "Seafood",
+		 type: 'food'
+		},{
+	
+		 name: "Beverages",
+		 type: 'food'
+		},{
+	
+		 name: "Baked goods",
+		 type: 'food'
+		},{
+	
+		 name: "Baking",
+		 type: 'food'
+		},{
+	
+		 name: "Snacks",
+		 type: 'food'
+		},{
+	
+		 name: "Themed meals",
+		 type: 'food'
+		},{
+	
+		 name: "Baby stuff",
+		 type: 'non-food'
+		},{
+	
+		 name: "Pets",
+		 type: 'non-food'
+		},{
+	
+		 name: "Fresh fruits",
+		 type: 'food'
+		},{
+	
+		 name: "Refrigerated items",
+		 type: 'food'
+		},{
+	
+		 name: "Frozen",
+		 type: 'food'
+		},{
+	
+		 name: "Various groceries",
+		 type: 'food'
+		},{
+	
+		 name: "Canned foods",
+		 type: 'food'
+		},{
+	
+		 name: "Spices & herbs",
+		 type: 'food'
+		},{
+	
+		 name: "Personal care",
+		 type: 'household'
+		},{
+	
+		 name: "Medicine",
+		 type: 'household'
+		},{
+	
+		 name: "Kitchen",
+		 type: 'household'
+		},{
+	
+		 name: "Other",
+		 type: 'household'
+		},{
+	
+		 name: "Cleaning products",
+		 type: 'household'
+		},{
+	
+		 name: "Office supplies",
+		 type: 'household'
+		},{
+	
+		 name: "Carcinogens",
+		 type: 'household'
+		},{
+	
+		 name: "Other stuff",
+		 type: 'household'
+		},{
+	
+		 name: "To-do list",
+		 type: 'household'
 		}
 	];
 
