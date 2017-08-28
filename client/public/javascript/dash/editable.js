23f526cc3f5fa8639717387ff5d5e55870c589f6
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-08-28 22:59:05 +0300

    +

--- client/public/javascript/dash/editable.js
+++ client/public/javascript/dash/editable.js
@@ -16,4 +16,15 @@
   // ----------------------
   $('#editableTable').editableTableWidget().numericInputExample().find('td:first').focus();
 
+	$('#editableTable td').on('change', function(event, newValue) {
+
+		// do something with the new cell value 
+		console.log(event);
+		console.log(newValue);
+		// if (....) { 
+			// return false; // reject change
+		// }
+
+	});
+
 })(document, window, jQuery);
