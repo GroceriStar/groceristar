23e13f3299296d20ee7f7f7b37764d23143a85a9
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-09-04 13:38:27 +0300

    +

--- server/boot/favorites-router.js
+++ server/boot/favorites-router.js
@@ -27,7 +27,7 @@ module.exports = function(app) {
 
   });
 
-  // :todo not sure, do we need this ensureLogin, huh?
+
   router.post('/delete/favorites/:ingredientId', 
     ensureLoggedIn('/auth/account'), function(req, res, next) {
 
@@ -48,26 +48,26 @@ module.exports = function(app) {
     };
     User.proceed(options);
 
-    User.getCurrentUserWithFavorites(req.user.id, function(err, model) {
+    // User.getCurrentUserWithFavorites(req.user.id, function(err, model) {
 
-      var data = model.toJSON();
-      console.log(data.favorites);
+    //   var data = model.toJSON();
+    //   console.log(data.favorites);
 
-      if( !data.favorites ){ return true; } //:todo test this
+    //   if( !data.favorites ){ return true; } //:todo test this
 
-      let forDeletion = [ req.favoriteId ];
+    //   let forDeletion = [ req.favoriteId ];
 
-      let arr = data.favorites;
+    //   let arr = data.favorites;
 
-      arr = arr.filter(item => !forDeletion.includes(item))
-      // !!! Read below about array.includes(...) support !!!
+    //   arr = arr.filter(item => !forDeletion.includes(item))
+    //   // !!! Read below about array.includes(...) support !!!
 
-      console.log(arr);
+    //   console.log(arr);
 
-      model.updateAttribute('favs', arr);
-      console.log(model);
+    //   model.updateAttribute('favs', arr);
+    //   console.log(model);
 
-    })
+    // })
 
   });
 
@@ -79,7 +79,7 @@ module.exports = function(app) {
 
   // });
 
-  router.get('/add/fav2/:ingredientId', function(req, res, next) {
+  router.get('/add/fav2/:ingredientId', ensureLoggedIn('/auth/account'), function(req, res, next) {
 
     var ingredients = req.params.ingredientId;
     var userId       = req.user.id;
@@ -90,9 +90,8 @@ module.exports = function(app) {
     var User = app.models.user;
 
     // this is a duplicated function from Grocery :todo think about it, real talk
-    
-    var options = {
- type  : 'add',
+     var options = {
+      type  : 'add',
       field : 'favs',
       userId: userId,
       secondArray: ingredients 
@@ -105,6 +104,26 @@ module.exports = function(app) {
 
   });
 
+  router.get('/add/fav2/clear', function(req, res, next) {
+    var userId       = req.user.id;
+
+    // console.log( ingredientId );
+    // console.log( userId );
+
+    var User = app.models.user;
+
+    // this is a duplicated function from Grocery :todo think about it, real talk   
+    var options = {
+      type  : 'clear',
+      field : 'favs',
+      userId: userId,
+    };
+    User.proceed(options);
+
+    res.redirect('/auth/account');
+
+
+  });
 
 
 	app.use(router);
