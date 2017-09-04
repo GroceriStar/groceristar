429cd371fecbf631ac453800a449d5d6c4875aa4
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-09-04 18:19:35 +0300

    +

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,30 @@
-webpack.config.js
+var webpack = require('webpack');
+const path  = require('path');
+
+var paths = {
+    projectRoot: __dirname,
+    appRoot: path.join(__dirname, 'server'),
+    buildDir: 'build',
+    buildRoot: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
+
+
+// module.exports = {
+//     context: paths.projectRoot,
+//     entry: './server/server.js',
+//     target: 'node',
+//     devtool: 'source-map',
+// //        output: {
+// //   filename: 'bundle.js',
+// //   path: path.resolve(__dirname, 'dist')
+// // }
+    
+// };
