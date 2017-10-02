796706a7bbf9e64609166c86f001201d855fc5ce
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-10-03 00:38:29 +0300

    +++

--- server/controllers/department-controller.js
+++ server/controllers/department-controller.js
@@ -18,20 +18,80 @@ exports.departmentsList = async (req, res, next) => {
 	var Grocery   = app.models.Grocery;
 
 	let grocery
-      try {      
-         var Grocery   = app.models.Grocery;
-         // grocery = await Grocery.fetchById(groceryId);
-         grocery  = await Grocery.findById(groceryId, Grocery.query1());
-         response = Grocery.convertCollectionData(grocery);
+  let response
+  try {      
+     var Grocery   = app.models.Grocery;
+     // grocery = await Grocery.fetchById(groceryId);
+     grocery  = await Grocery.findById(groceryId, Grocery.query1());
+     response = Grocery.convertCollectionData(grocery);
+     // console.log(response);
 
+  } catch (e) {
+    //this will eventually be handled by your error handling middleware
+    next(e) 
+  }
 
-      } catch (e) {
-        //this will eventually be handled by your error handling middleware
-        next(e) 
+  // departments: response.data,
+  var renderObject = {
+    name: response.name,     
+
+    departments : response,    
+    // description : d.desc,
+    groceryId   : groceryId
+  };
+
+  console.log(renderObject)
+
+      
+  // res.render('pages/change-grocery-list-name', renderObject);
+
+};
+
+exports.deleteDepartment = async (req, res, next) => {
+
+  var departmentId = req.params.id;
+  var groceryId    = req.params.groceryId;
+
+  var Grocery      = app.models.Grocery;
+    
+  
+
+    // :todo update things. This is a duplicated code
+    Grocery.findById(groceryId, {
+      include: {
+        relation: 'ingredients',
+        scope: {
+          where: {
+            departmentId: departmentId
+          }
+        }
       }
 
-// departments: response.data,
-res.render('pages/change-grocery-list-name', {});
+    }, function(err, grocery){
+
+      console.log(grocery);
+
+      var g = grocery.toJSON();
+
+      var toRemove = _.pluck(g.ingredients, 'id');
+      toRemove     = _.map(toRemove, item => item.toString());
+
+      console.log(toRemove);
+
+
+
+      var options = {
+          groceryId: groceryId,
+          secondArray: toRemove
+        };
+            
+      Grocery.removeIngredient(options);
+
+      // Grocery.removeRemoveDepartment(options);
+
+    });
+
+    res.redirect('/view/grocery/' + groceryId);
 
 };
 
