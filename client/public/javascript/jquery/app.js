5cc2324f242586f3d39037202105564c09a271f0
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-10-10 02:14:32 +0300

    +++

--- client/public/javascript/jquery/app.js
+++ client/public/javascript/jquery/app.js
@@ -10,22 +10,7 @@ jQuery(function ($) {
 
 		pluralize: function (count, word) {
 			return count === 1 ? word : word + 's';
-		},
-
-
-		// store: function (namespace, data) {
-		// 	if (arguments.length > 1) {
-		// 		// console.log('added');
-		// 		// return localStorage.setItem(namespace, JSON.stringify(data));
-
-		// 	} else {
-
-		// 		var store = localStorage.getItem(namespace);
-		// 		return (store && JSON.parse(store)) || [];
-
-		// 	}
-		// },
-
+		}
 
 		
 	};
@@ -33,7 +18,10 @@ jQuery(function ($) {
 
 
 	var App = {
-		
+		isUltimate: function(){
+			var flag = $('body').data().isUltimate;
+			return flag;			
+		},
 		getDepartmentId: function(){
 			var departmentId = $('#new-todo').data().departmentId;
 			return departmentId;			
