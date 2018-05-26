f6c15b93943c783d70772302ee424f69f69d42cb
Arthur Tkachenko <atherdon@gmail.com>
2018-05-26 15:34:58 +0300

    removing static pages

--- server/boot/index-router.js
+++ server/boot/index-router.js
@@ -18,12 +18,6 @@ module.exports = function(app) {
   var router  = app.loopback.Router();
   var mainController = require('../controllers/main-controller');
   
-  // routers 
-
-  
-
-
-
 
 
   // :todo this must be moved to departments
@@ -49,19 +43,8 @@ module.exports = function(app) {
 
 
 
-  router.get('/credits', mainController.getCreditsPage);
-
-  router.get('/privacy', mainController.getPrivacyPage);
-
-  router.get('/terms', mainController.getTermsPage);
-
   router.get('/', mainController.getHomepage);
 
-
-
-  //  app.use(function(req, res, next) {
-  //   res.status(404).send('Sorry cant find that!');
-  // });
 
   app.use(router);
 
